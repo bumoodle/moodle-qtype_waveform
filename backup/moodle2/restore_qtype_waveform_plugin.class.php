d65a5bc44ed9ae4053c8c1dd0946c0a3b1dfa0c7
Kyle J. Temkin <ktemkin@binghamton.edu>
2014-07-08 00:56:58 -0400

    Fix backup and restore issues. (#1)

--- backup/moodle2/restore_qtype_waveform_plugin.class.php
+++ backup/moodle2/restore_qtype_waveform_plugin.class.php
@@ -1,5 +1,4 @@
 <?php
-
 // This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
@@ -22,30 +21,23 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+
 defined('MOODLE_INTERNAL') || die();
 
 
 /**
  * restore plugin class that provides the necessary information
- * needed to restore one boolean qtype plugin
+ * needed to restore one waveform qtype plugin
+ *
+ * @copyright  2010 onwards Eloy Lafuente (stronk7) {@link http://stronk7.com}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class restore_qtype_waveform_plugin extends restore_qtype_plugin 
-{
-	
- 	//~ktemkin
- 	//ease of use fields
- 	const base_name = 'waveform';
- 	const base_db_name =  'question_waveform';
- 	const base_extra_fields = 'wave|autofeeback|grademethod';
- 	const base_questionid_field = 'question';
- 	
-
+class restore_qtype_waveform_plugin extends restore_qtype_plugin {
 
     /**
      * Returns the paths to be handled by the plugin at question level
      */
-    protected function define_question_plugin_structure() 
-    {
+    protected function define_question_plugin_structure() {
 
         $paths = array();
 
@@ -53,56 +45,37 @@ class restore_qtype_waveform_plugin extends restore_qtype_plugin
         $this->add_question_question_answers($paths);
 
         // Add own qtype stuff
-        $elename = self::base_name;
-        $elepath = $this->get_pathfor(self::base_name); // we used get_recommended_name() so this works
+        $elename = 'waveform';
+        // we used get_recommended_name() so this works
+        $elepath = $this->get_pathfor('/waveform');
         $paths[] = new restore_path_element($elename, $elepath);
 
         return $paths; // And we return the interesting paths
     }
 
     /**
-     * Process the qtype/boolean element
+     * Process the qtype/waveform element
      */
-    public function process_waveform($data) 
-    {
+    public function process_waveform($data) {
         global $DB;
 
         $data = (object)$data;
         $oldid = $data->id;
-        
+
         // Detect if the question is created or mapped
         $oldquestionid   = $this->get_old_parentid('question');
         $newquestionid   = $this->get_new_parentid('question');
         $questioncreated = $this->get_mappingid('question_created', $oldquestionid) ? true : false;
 
-        // If the question has been created by restore, we need to create its question_waveform too
-        if ($questioncreated)
-         {
+        // If the question has been created by restore, we need to create its
+        // question_waveform entry too
+        if ($questioncreated) {
             // Adjust some columns
             $data->question = $newquestionid;
-
-            /*
-
-            XXX test me out?
-
-            // Map sequence of question_answer ids
-            $answersarr = explode(',', $data->answers);
-            foreach ($answersarr as $key => $answer) 
-                $answersarr[$key] = $this->get_mappingid('question_answer', $answer);
-            $data->answers = implode(',', $answersarr);
-
-            */
-            
             // Insert record
-            $newitemid = $DB->insert_record(self::base_db_name, $data);
-            
+            $newitemid = $DB->insert_record('question_waveform', $data);
             // Create mapping
-            $this->set_mapping(self::base_db_name, $oldid, $newitemid);
-            
-        } 
-        else 
-        {
-            // Nothing to remap if the question already existed
+            $this->set_mapping('question_waveform', $oldid, $newitemid);
         }
     }
 }
